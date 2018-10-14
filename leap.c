81bfbf9d11792e891d9977e37fcad87c860b711e
preethakarthi <42778213+preethakarthi@users.noreply.github.com>
2018-10-15 00:07:37 +0530

    Add files via upload

---
+++ leap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int y;
+ printf("enter year");
+ scanf("%d",&y);
+ if((y%4==0)&&(y%100!=0)||(y%400==0))
+ {
+     printf("%d is leap year",y);
+ }
+ else
+ {
+     printf("%d is not leap year",y);
+ }
+}
